e0997735ec8ea2c8542a57c2cb8a3f488e98c8b0
ScreamingSUN13 <lilmonsterkellystephen@gmail.com>
2026-04-10 23:43:27 -0400

    Synced lua line

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,26 +10,15 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
-require("lazy").setup({
-  { --Theme
-    "EdenEast/nightfox.nvim",
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme terafox")
-    end,
-  },
-  { "hrsh7th/nvim-cmp" },          -- the engine itself
-  { "L3MON4D3/LuaSnip" },          -- cmp requires a snippet engine
-  { "saadparwaiz1/cmp_luasnip" },  -- links luasnip to cmp
-  { "hrsh7th/cmp-nvim-lsp" },  -- completions from LSP (recommended if using LSP)
-  { "hrsh7th/cmp-buffer" },    -- completions from words in current file
-  { "hrsh7th/cmp-path" },      -- completions for file paths
-  { "nvim-treesitter/nvim-treesitter" }, -- better syntax highlighting
-})
+require("lazy").setup("plugins")
 
--- Transparency
+-- Themeing
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
--- Clipboard support
-vim.opt.clipboard = "unnamedplus"
+local currentLuaLineTheme = require"themes.mainLuaLine"
+
+require('lualine').setup {
+	options = { theme = currentLuaLineTheme }
+}
+
