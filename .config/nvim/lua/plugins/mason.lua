c390d505f79cc129d40c6631eb32c513d90d2401
Stephen Kelly <stephenkelly@Stephens-MacBook-Air.local>
2026-04-11 19:35:37 -0400

    Added Yazi support in vim by doing <space> y

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,24 +1,35 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "pyright", "ts_ls" },
-      automatic_installation = true,
-    })
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "pyright", "ts_ls" },
+			automatic_installation = true,
+		})
 
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    local servers = { "lua_ls", "pyright", "ts_ls" }
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					diagnostics = {
+			globals = { "vim" },	-- tell lua_ls that vim is a valid global
+					},
+				},
+			},
+		})
+		vim.lsp.enable("lua_ls")
 
-    for _, server in ipairs(servers) do
-      vim.lsp.config(server, {
-        capabilities = capabilities,
-      })
-      vim.lsp.enable(server)
-    end
-  end,
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local servers = { "lua_ls", "pyright", "ts_ls" }
+
+		for _, server in ipairs(servers) do
+			vim.lsp.config(server, {
+	capabilities = capabilities,
+			})
+			vim.lsp.enable(server)
+		end
+	end,
 }
