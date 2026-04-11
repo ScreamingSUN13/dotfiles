909036978dd9bcc96d304dd119d48866f8ef8154
ScreamingSUN13 <lilmonsterkellystephen@gmail.com>
2026-04-10 20:14:47 -0400

    Added wallpaper to wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,15 @@
+local wezterm = require 'wezterm'
 
 return {
-  window_background_image = "~/Pictures/e07ed1f76e8f266b8e12b5a05d2dd15e.jpg",
-  window_background_image_hsb = {
-    brightness = 0.05,  -- darken the image so text is readable
-    saturation = 1.0,
-    hue = 1.0,
-  },
+	font = wezterm.font("JetBrains Mono"),
+	font_size = 13.0,
+	color_scheme = "Terafox",
+	window_background_opacity = 1,
+	hide_tab_bar_if_only_one_tab = true,
+	window_background_image = "/home/stephenk/Pictures/Wallpapers/e07ed1f76e8f266b8e12b5a05d2dd15e.jpg",
+	window_background_image_hsb = {
+		brightness = 0.1,  -- darken the image so text is readable
+		saturation = 1.0,
+		hue = 1.0,
+	},
 }
