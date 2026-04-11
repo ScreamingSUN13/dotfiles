4fbc90a3ad02ec5a9a476879232b5c3668f36016
Stephen Kelly <stephenkelly@Stephens-MacBook-Air.local>
2026-04-10 20:58:00 -0400

    Put this on my main user[C

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ return {
 	color_scheme = "Terafox",
 	window_background_opacity = 1,
 	hide_tab_bar_if_only_one_tab = true,
-	window_background_image = "/Users/School/Pictures/Wallpapers/e07ed1f76e8f266b8e12b5a05d2dd15e.jpg",
+	window_background_image = "/Users/stephenkelly/Pictures/Wallpapers/e07ed1f76e8f266b8e12b5a05d2dd15e.jpg",
 	window_background_image_hsb = {
 		brightness = 0.1,  -- darken the image so text is readable
 		saturation = 1.0,
