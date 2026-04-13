e77e720fadc717490370634bb3a82bf115e89a4d
Stephen Kelly <stephenkelly@Stephens-MacBook-Air.local>
2026-04-12 21:27:31 -0400

    Fixed the RAM symbol

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 
 return {
-	font = wezterm.font("JetBrains Mono"),
+	font = wezterm.font("JetBrainsMono Nerd Font"),
 	font_size = 13.0,
 	color_scheme = "Terafox",
 	window_background_opacity = 1,
