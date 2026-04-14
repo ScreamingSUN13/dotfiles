9a0f2dab1596737e72c1ba15e98225f1ff7c3640
Stephen Kelly <stephenkelly@Stephens-MacBook-Air.local>
2026-04-14 10:26:26 -0400

    Made edits to the colors of fastfetch

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,7 @@ return {
 	color_scheme = "Terafox",
 	window_background_opacity = 1,
 	hide_tab_bar_if_only_one_tab = true,
-	window_background_image = "/Users/stephenkelly/Pictures/Wallpapers/blueOuterWilds.jpg",
+	window_background_image = "/Users/stephenkelly/Pictures/Wallpapers/nvimOuterWilds.jpg",
 	window_background_image_hsb = {
 		brightness = 0.8,  -- darken the image so text is readable
 		saturation = 0.9,
