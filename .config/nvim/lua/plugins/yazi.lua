afd5d3fbb6d25e841f0a64cee2a25b63682e4321
Stephen Kelly <stephenkelly@Stephens-MacBook-Air.local>
2026-04-15 10:03:05 -0400

    Added animation and which-key for nvim

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -4,10 +4,21 @@ return {
   config = function()
     require("yazi").setup({
       open_for_directories = true,  -- open yazi when nvim is opened on a directory
+
+			-- open in a floating window
+      floating_window_scaling_factor = 0.9, -- how much of the screen it takes up
+
+      -- keep nvim and yazi in sync
+      sync_buffer_on_close = true,          -- switch to file when yazi closes
+      open_multiple_tabs = false,           -- open multiple files as splits instead
+
+      -- keymaps inside yazi
+      keymaps = {
+        show_help = "~",
+			},
     })
   end,
   keys = {
-    { "<leader>y", "<cmd>Yazi<cr>",        desc = "Open yazi at current file" },
-    { "<leader>Y", "<cmd>Yazi cwd<cr>",    desc = "Open yazi at working directory" },
+    { "y", "<cmd>Yazi<cr>",        desc = "Open yazi at current file" },
   },
 }
