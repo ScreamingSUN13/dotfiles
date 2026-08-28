0a712475b9f70044da8568ae688718195d1ea0b6
ScreamingSUN13 <lilmonsterkellystephen@gmail.com>
2026-08-28 17:38:04 -0400

    Added lualine syncing with noctalia

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()				
-				local currentLuaLineTheme = require("themes.mainLuaLine")
+				local currentLuaLineTheme = require("themes.noctalia")
 				require('lualine').setup {
 						options = { theme = currentLuaLineTheme }
 				}
